150ea4fe5bd3ca817a9ea59ef6c181c3f70ca591
CI-Esther.Chen <>
2020-09-11 15:14:37 +0800

    Update HomePage with image and text

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_app/firebase/UserAuth.dart';
+import 'package:flutter_app/model/Post.dart';
+import 'package:flutter_app/widgets/PostWidget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -44,15 +46,22 @@ class _HomePageState extends State<HomePage> {
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(body: SafeArea(child: Center(child: CircularProgressIndicator())));;
+          return Scaffold(body: SafeArea(child: Center(child: CircularProgressIndicator())));
         }
-
         return new ListView(
           children: snapshot.data.docs.map((DocumentSnapshot document) {
-            return new ListTile(
-              title: new Text(document.data()['authorUID']),
-              subtitle: new Text(document.data()['imageURL']),
-            );
+            return new Container(child:
+                new Column(children: [
+                  new Image.network(
+                      document.data()['imageURL'],
+                      width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                    fit:BoxFit.cover
+                  ),
+                  new Text(document.data()['email'])
+                ],
+                )
+            ) ;
           }).toList(),
         );
       },
