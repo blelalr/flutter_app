457dcd973885d47a789915237f294f05f9bc46ec
CI-Esther.Chen <>
2020-09-15 18:09:29 +0800

    Add PageView at HomePage Use Bottom Navigation Bar

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,10 +1,13 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_app/firebase/UserAuth.dart';
-import 'package:flutter_app/model/Post.dart';
-import 'package:flutter_app/widgets/PostWidget.dart';
+import 'package:flutter_app/page/NotificationsPage.dart';
+import 'package:flutter_app/page/ProfilePage.dart';
+import 'package:flutter_app/page/SearchPage.dart';
+import 'package:flutter_app/page/TimeLinePage.dart';
+import 'package:flutter_app/page/UploadPage.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,10 +17,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController pageController;
+  int currentPageIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
-
-
     return SafeArea(
         child: new Scaffold(
           appBar: new AppBar(
@@ -32,50 +47,51 @@ class _HomePageState extends State<HomePage> {
                 'Flutter App',
                 style: GoogleFonts.playball(fontSize: 30),
               )),
-          body: homePageBody(),
+          body: buildHomeBody(),
+          bottomNavigationBar: buildHomeBottom(),
         ));
   }
 
-  Widget homePageBody() {
-    CollectionReference posts = FirebaseFirestore.instance.collection('Posts');
-    return StreamBuilder<QuerySnapshot>(
-      stream: posts.snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
+  onPageChangeListener(int pageIndex){
+    setState(() {
+      this.currentPageIndex = pageIndex;
+    });
+  }
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(body: SafeArea(child: Center(child: CircularProgressIndicator())));
-        }
-        return new ListView(
-          children: snapshot.data.docs.map((DocumentSnapshot document) {
-            return new Container(child:
-                new Column(children: [
-                  headerView(Post.fromDocument(document)),
-                  contentView(Post.fromDocument(document)),
-                  // footerView(Post.fromDocument(document)),
-
-                ],
-                )
-            ) ;
-          }).toList(),
-        );
-      },
+  Widget buildHomeBody() {
+    return PageView(
+      children: [
+        TimeLinePage(),
+        SearchPage(),
+        UploadPage(),
+        NotificationsPage(),
+        ProfilePage()
+      ],
+      controller: pageController,
+      onPageChanged: onPageChangeListener,
+      physics: NeverScrollableScrollPhysics(),
     );
   }
 
-  Widget headerView(Post post) {
-
-    return Text(post.authorUID);
+  Widget buildHomeBottom() {
+    return CupertinoTabBar(
+      currentIndex: currentPageIndex,
+      onTap:onTapPageChange,
+      activeColor: Colors.black54,
+      inactiveColor: Colors.black12,
+      backgroundColor: Colors.white,
+      items: [
+        BottomNavigationBarItem(icon:Icon(Icons.home)),
+        BottomNavigationBarItem(icon:Icon(Icons.search)),
+        BottomNavigationBarItem(icon:Icon(Icons.photo_camera)),
+        BottomNavigationBarItem(icon:Icon(Icons.favorite)),
+        BottomNavigationBarItem(icon:Icon(Icons.person)),
+      ],
+    );
   }
 
-  Widget contentView(Post post) {
-    return new Image.network(
-        post.imageURL,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.width*3/4,
-        fit:BoxFit.cover
-    );
+
+  onTapPageChange(int pageIndex) {
+    pageController.animateToPage(pageIndex, duration: Duration(milliseconds: 400), curve:Curves.bounceInOut);
   }
 }
--- lib/page/TimeLinePage.dart
+++ lib/page/TimeLinePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_app/model/Post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class TimeLinePage extends StatefulWidget {
   @override
   _TimeLinePageState createState() => _TimeLinePageState();
@@ -8,6 +11,49 @@ class TimeLinePage extends StatefulWidget {
 class _TimeLinePageState extends State<TimeLinePage> {
   @override
   Widget build(context) {
-    return Text("TimeLine Page goes here.");
+    return buildTimeLinePage();
+  }
+
+  Widget buildTimeLinePage() {
+    CollectionReference posts = FirebaseFirestore.instance.collection('Posts');
+    return StreamBuilder<QuerySnapshot>(
+      stream: posts.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: SafeArea(child: Center(child: CircularProgressIndicator())));
+        }
+        return new ListView(
+          children: snapshot.data.docs.map((DocumentSnapshot document) {
+            return new Container(child:
+            new Column(children: [
+              headerView(Post.fromDocument(document)),
+              contentView(Post.fromDocument(document)),
+              // footerView(Post.fromDocument(document)),
+
+            ],
+            )
+            ) ;
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  Widget headerView(Post post) {
+
+    return Text(post.authorUID);
+  }
+
+  Widget contentView(Post post) {
+    return new Image.network(
+        post.imageURL,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width*3/4,
+        fit:BoxFit.cover
+    );
   }
 }
