145e6c5665700ef0e27e0858ed066148c4ae1ed2
CI-Esther.Chen <>
2020-09-26 15:00:55 +0800

    Update SearchList UI

--- lib/page/SearchPage.dart
+++ lib/page/SearchPage.dart
@@ -22,242 +22,90 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      children: tempArray
-          .asMap()
-          .map((index, type) {
-            return MapEntry(index,
-                new Container(child: UserResult(tempArray, index, type)));
+    return new Container(
+      color: Colors.white,
+      child: new ListView.builder(
+          itemCount: tempArray.length,
+          itemBuilder: (BuildContext context, int index ) {
+            if (tempArray[index] == "U") {
+              return unreadItemView(context);
+            } else {
+              return seenItemView(context);
+            }
           })
-          .values
-          .toList(),
     );
-  }
-}
-
-class UserResult extends StatelessWidget {
-  int _index;
-  String _type;
-  List<String> _tempArray;
-
-  UserResult(this._tempArray, this._index, this._type);
-
-  @override
-  Widget build(BuildContext context) {
-    if (_type == "U") {
-      return unreadItemView(context);
-    } else {
-      return seenItemView(context);
-    }
   }
 
   Widget unreadItemView(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          height: 100.0,
-          width: MediaQuery.of(context).size.width,
-          color: Colors.white,
-        ),
-        Container(
-          height: 100.0,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-                0.1,
-                1
-              ], colors: [
-                // Colors.red,
-                // Colors.orange,
-                Color(int.parse('FFF3F6FA', radix: 16)),
-                Color(int.parse('FFFFFFFF', radix: 16)),
-              ]),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(42.0),
-                topRight: Radius.zero,
-                bottomLeft: Radius.zero,
-                bottomRight: Radius.circular(42.0),
-              )),
-        ),
-        Container(
-          height: 100.0,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-                0.1,
-                1
-              ], colors: [
-                // Colors.orange,
-                // Colors.red
-                Color(int.parse('FFFFFFFF', radix: 16)),
-                Color(int.parse('FFF3F6FA', radix: 16)),
-              ]),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.zero,
-                topRight: Radius.circular(42.0),
-                bottomLeft: Radius.circular(42.0),
-                bottomRight: Radius.zero,
-              )),
-        ),
-      ],
+    return new Align(
+        alignment: Alignment.topLeft,
+        heightFactor: 0.5,
+          child: new ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                  height: 180.0,
+                  width: MediaQuery.of(context).size.width,
+                  // color: Colors.blue,
+                  decoration: BoxDecoration(
+                      gradient:
+                          LinearGradient(begin: Alignment.topRight, stops: [
+                    0.5,
+                    1
+                  ], colors: [
+                    // Colors.red,
+                    // Colors.orange,
+                    Color(int.parse('FFF3F6FA', radix: 16)),
+                    Color(int.parse('FFFFFFFF', radix: 16)),
+                  ]))))
     );
   }
 
   Widget seenItemView(BuildContext context) {
-    if (_tempArray[_index - 1] == "U" && _tempArray[_index + 1] == "S") {
-      return Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            height: 100.0,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.white,
-          ),
-          Container(
-            height: 100.0,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                color: Color(int.parse('FFFFFFFF', radix: 16)),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(42.0),
-                  topRight: Radius.zero,
-                  bottomLeft: Radius.zero,
-                  bottomRight: Radius.circular(42.0),
-                )),
-          ),
-          Container(
-            height: 100.0,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                color: Color(int.parse('FFF3F6FA', radix: 16)),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.zero,
-                  topRight: Radius.circular(42.0),
-                  bottomLeft: Radius.circular(42.0),
-                  bottomRight: Radius.zero,
-                )),
-          ),
-        ],
-      );
-    }
-    else {
-      return Stack(
-        children: [
-          Container(
-            child: Flex(
-              direction: Axis.horizontal,
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Stack(
-                    alignment: Alignment.topCenter,
-                    children: [
-                      Container(
-                        height: 100.0,
-                        color: Color(int.parse('FFF3F6FA', radix: 16)),
-                      ),
-                      Container(
-                        height: 100.0,
-                        decoration: BoxDecoration(
-                            color: Color(int.parse('FFF3F6FA', radix: 16)),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.zero,
-                              topRight: Radius.zero,
-                              bottomLeft: Radius.circular(32.0),
-                              bottomRight: Radius.zero,
-                            ),
-                            border: Border.all(
-                                width: 4,
-                                color: Color(int.parse('FFFFFFFF', radix: 16)),
-                                style: BorderStyle.solid)),
-                      ),
-                      Container(
-                        height: 4.0,
-                        color: Color(int.parse('FFF3F6FA', radix: 16)),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    height: 100,
-                    color: Color(int.parse('FFF3F6FA', radix: 16)),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Container(
-                        height: 100.0,
-                        color: Color(int.parse('FFF3F6FA', radix: 16)),
-                      ),
-                      Container(
-                        height: 100.0,
-                        decoration: BoxDecoration(
-                            color: Color(int.parse('FFF3F6FA', radix: 16)),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.zero,
-                              topRight: Radius.circular(32.0),
-                              bottomLeft: Radius.zero,
-                              bottomRight: Radius.zero,
-                            ),
-                            border: Border.all(
-                                width: 4,
-                                color: Color(int.parse('FFFFFFFF', radix: 16)),
-                                style: BorderStyle.solid)),
-                      ),
-                      Container(
-                        height: 4.0,
-                        color: Color(int.parse('FFF3F6FA', radix: 16)),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // Container(
-          //   child: Flex(
-          //     direction: Axis.horizontal,
-          //     children: <Widget>[
-          //       Expanded(
-          //         flex: 1,
-          //         child: Container(
-          //           alignment: Alignment.topRight,
-          //           height: 100.0,
-          //           color: Colors.transparent,
-          //         ),
-          //       ),
-          //       Expanded(
-          //           flex: 5,
-          //           child: Stack(alignment: Alignment.topCenter, children: [
-          //             Container(
-          //               height: 100,
-          //               color: Color(int.parse('FFF3F6FA', radix: 16)),
-          //             ),
-          //             Container(
-          //               height: 4,
-          //               color: Color(int.parse('FFFFFFFF', radix: 16)),
-          //             ),
-          //           ])),
-          //       Expanded(
-          //         flex: 1,
-          //         child: Container(
-          //           alignment: Alignment.topRight,
-          //           height: 100.0,
-          //           color: Colors.transparent,
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // )
-        ],
-      );
-    }
+    return new Align(
+        alignment: Alignment.topLeft,
+        heightFactor: 0.58,
+        child: new ClipPath( clipper:  MyClipper(),
+        child: Container(
+          height: 180.0,
+          color: Color(int.parse('FFF3F6FA', radix: 16))
+        ))
+    );
   }
 }
+
+class MyClipper extends CustomClipper<Path>{
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.moveTo(size.width - 40, 40);
+    //從右上角開始
+    path.lineTo(40, 40);
+    //向左連線至曲線起始點 //上直線
+    path.quadraticBezierTo(0, 40, 0, 0);
+    //畫出左上角的曲線
+    path.lineTo(0, size.height - 80);
+    //向下連線至曲線起始點 //左直線
+    path.quadraticBezierTo(0, size.height - 40, 40,  size.height - 40);
+    //畫出左下角的曲線
+    path.lineTo(size.width - 40, size.height - 40);
+    //向右連線至曲線起始點 //下直線
+    path.quadraticBezierTo(size.width, size.height - 40, size.width,  size.height);
+    //畫出右下角的曲線
+    path.lineTo(size.width , 80);
+    //向上連線至曲線起始點 //右直線
+    path.quadraticBezierTo(size.width, 40, size.width - 40,  40);
+    //畫出右上角的曲線
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
+  }
+
+
+  }
+
